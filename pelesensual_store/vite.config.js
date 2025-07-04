03c878e6422ba5b53a53e16a04b3b0dfdce3373b
frankwss7 <frankwss80@gmail.com>
2025-07-04 00:52:00 -0300

    Update vite.config.js

--- pelesensual_store/vite.config.js
+++ pelesensual_store/vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Importante para o Vercel
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true
+  },
+  publicDir: 'public' // Garante que assets públicos sejam copiados
 })
