41d3e67eb352e9c32fb7e4804f92222d33900771
frankwss7 <frankwss80@gmail.com>
2025-07-04 01:13:58 -0300

    Update ProductGrid.js

--- pelesensual_store/components/ProductGrid.js
+++ pelesensual_store/components/ProductGrid.js
@@ -3,7 +3,7 @@ import { products } from '../data/products';
 
 const ProductGrid = () => {
   return (
-    <div className="products-grid">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 p-6">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
